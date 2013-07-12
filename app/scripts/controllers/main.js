3185a50eb47878d8daaa1f6b883d6cb4740c815a
Brandon Brown <brandonb@redhillstudios.com>
2013-07-12 15:59:44 -0700

    more progress on markup css also have d3 placeholder graphs working with styled axis labels

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,4 +9,25 @@ angular.module('econv4protoApp')
       'AngularJS',
       'Karma'
     ];
+
+	$scope.data = [
+	  {x: 0, value: 4, otherValue: 14},
+	  {x: 1, value: 8, otherValue: 1},
+	  {x: 2, value: 15, otherValue: 11},
+	  {x: 3, value: 16, otherValue: 147},
+	  {x: 4, value: 23, otherValue: 87},
+	  {x: 5, value: 42, otherValue: 45}
+	]; 
+
+	$scope.options = {
+	  axes: {
+	    x: {type: 'linear', tooltipFormatter: function(x) {return x;}}
+	  },
+	  series: [
+	    {y: 'value', color: 'blue', label: 'Production (lbs)'},
+	    {y: 'otherValue', color: 'red', label: 'Consumption (lbs)'}
+	  ],
+	  //lineMode: 'linear'
+	}
+    
   });
