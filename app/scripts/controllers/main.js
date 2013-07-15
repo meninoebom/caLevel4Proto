0882ee0454bc37ac2f96e7e0b04bbe29a7127d66
Brandon Brown <brandonb@redhillstudios.com>
2013-07-15 15:47:41 -0700

    rudimentary databinding working for trade slider

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,15 +2,44 @@
 
 angular.module('econv4protoApp')
   .controller('MainCtrl', function ($scope) {
+
+  	window.scope = $scope;
+
     $scope.initialWood = 5;
     $scope.studentProductionWood = 48;
     $scope.studentProductionFish = 0;
+    
     $scope.barterPriceWood = 1;
-    $scope.barterPriceFish = 125;
-    $scope.minStudentTradeWood = 0;
-    $scope.maxStudentTradeWood = 288;
+    $scope.barterPriceFish = 125;     
+    $scope.barterPrice = function() {
+    	return $scope.barterPriceWood/$scope.barterPriceFish; //bp
+    }
 
+    $scope.minStudentTradeWood = 0; //minstw
+    $scope.maxStudentTradeWood = 48; //maxstw
 
+    $scope.fishTradedToFriday = 90; //ftf
+    $scope.fishTradedFromFriday = 1875; //fff
+    $scope.woodTradedToFriday = 48;
+   // $scope.woodTradedToFriday = function() {	//wtf
+    	//return $scope.studentProductionWood - $scope.studentConsumptionWood; 
+    //}
+    $scope.woodTradedFromFriday = 90; //wff
+    $scope.studentConsumptionWood = 33;	//scw
+    // $scope.studentConsumptionWood = function() {	//scw
+    // 	return $scope.studentProductionWood - $scope.woodTradedToFriday;
+    // }
+    $scope.studentConsumptionFish = $scope.fishTradedFromFriday;
+
+    $scope.minStudentConsumptionWood = 0; //minscw
+    $scope.maxStudentConsumptionWood = 48; //maxscw
+    $scope.minStudentConsumptionFish = 0; //minscf
+    $scope.maxStudentConsumptionFish = 3600; //maxscf
+
+    $scope.traderConsumptionWood = $scope.woodTradedToFriday; //tcw
+    $scope.traderConsumptionFish = function() {
+    	return 3600 - $scope.fishTradedFromFriday; //tcf
+    }
 
 	$scope.data = [
 	  {x: 0, value: 4, otherValue: 14},
@@ -31,5 +60,14 @@ angular.module('econv4protoApp')
 	  ],
 	  //lineMode: 'linear'
 	}
+
+	$scope.$watch('woodTradedToFriday', function(newVal, oldVal){
+		$scope.studentConsumptionWood = $scope.maxStudentConsumptionWood - $scope.woodTradedToFriday;
+		if($scope.woodTradedToFriday * $scope.barterPriceFish <= $scope.maxStudentConsumptionFish) {
+			$scope.studentConsumptionFish = $scope.woodTradedToFriday * $scope.barterPriceFish;
+		} else {
+			$scope.studentConsumptionFish = 3600;			
+		}
+	});
     
   });
